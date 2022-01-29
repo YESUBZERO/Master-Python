74ae97e4ce040415f1f147af6e39b479a29b8d22
YESUBZERO <miguel.10hid@gmail.com>
2022-01-29 14:20:42 -0500

    Master-Python

--- 12-sistema-archivos/ficheros.py
+++ 12-sistema-archivos/ficheros.py
@@ -1,7 +1,7 @@
 from io import open
 import pathlib
 import shutil
-
+"""
 # Abrir archivo
 #ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
 #archivo = open(ruta, "a")
@@ -27,8 +27,32 @@ archivo_lectura.close()
 for elemento in lista:
     print(elemento)
 
-#  Copiar
+
+#  Copiar archivos usando libreria shutil
 ruta_original = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
 ruta_nueva = str(pathlib.Path().absolute()) + "/fichero_copiado.txt"
 #ruta_alternativa = "./01-holamundo/fichero_texto.txt"
 shutil.copyfile(ruta_original, ruta_nueva)
+
+
+# Mover archivo
+ruta_original = str(pathlib.Path().absolute()) + "/fichero_copiado.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/fichero_copiado_NUEVO.txt"
+shutil.move(ruta_original, ruta_nueva)
+
+
+# Eliminar archivos
+import os
+ruta_nueva = str(pathlib.Path().absolute()) + "/fichero_copiado_NUEVO.txt"
+os.remove(ruta_nueva)
+"""
+
+# Comprobar si un archivo existe
+import os.path
+#print(os.path.abspath("./"))
+ruta_comprobar = os.path.abspath("./") + "/fichero_texto.txt"
+
+if os.path.isfile(ruta_comprobar):
+    print("El archivo existe")
+else:
+    print("El archivo no existe")
