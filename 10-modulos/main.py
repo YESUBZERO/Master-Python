17c5a451e8abdb6e524d79f7d8004dee98112377
YESUBZERO <miguel.10hid@gmail.com>
2022-01-28 19:11:11 -0500

    Master-Python

---
+++ 10-modulos/main.py
@@ -0,0 +1,14 @@
+"""
+Modulos: son funcionalidades ya hechas para reutilizar.
+en python hay muchos por defectos en: #* https://docs.python.org/3/py-modindex.html
+
+Podemos conseguir modulos que ya vienen en el lenguaje, modulos en internet y tambien 
+podemos crear nuestros modulos.
+
+"""
+
+# importar mi propio modulo
+import modulo
+
+print(modulo.holamundo("Miguel"))
+
