1d28ab8beaf9c53f8714ebc966d582ac682d27d0
YESUBZERO <miguel.10hid@gmail.com>
2022-01-29 12:18:33 -0500

    Master-Python

--- 12-sistema-archivos/ficheros.py
+++ 12-sistema-archivos/ficheros.py
@@ -1,12 +1,34 @@
 from io import open
 import pathlib
+import shutil
+
+# Abrir archivo
+#ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+#archivo = open(ruta, "a")
+
+# Escribir
+#archivo.write("****** Soy un texto usando python ********\n")
+
+# Cerrar archivo
+#archivo.close()
 
 # Abrir archivo
 ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
-archivo = open(ruta, "a")
+archivo_lectura = open(ruta, "r")
 
-# Escribir
-archivo.write("****** Soy un texto usando python ********\n")
+# Leer contenido
+#contenido = archivo_lectura.read()
+#print(contenido)
 
-# Cerrar archivo
-archivo.close()
+# Leer contenido y guardarlo en lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+
+for elemento in lista:
+    print(elemento)
+
+#  Copiar
+ruta_original = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/fichero_copiado.txt"
+#ruta_alternativa = "./01-holamundo/fichero_texto.txt"
+shutil.copyfile(ruta_original, ruta_nueva)
